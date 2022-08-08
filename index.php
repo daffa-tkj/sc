ebd13714218f12a6af37372bd858318acb4d9018
daffa-tkj <109427521+daffa-tkj@users.noreply.github.com>
2022-08-08 21:18:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,58 @@
-<!DOCTYPE html>
+
+
 <html>
-<head>
-	<title>Hacked By User01_73r14r4ng</title>
-<link rel="icon" type="image/x-icon" href="https://cdn1-production-images-kly.akamaized.net/Pc5VhPpa8CNRHgFzwD6aCct7VTU=/0x0/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/1466271/original/574cab123cdc87f7263ebe24e64148a8-043677100_1483957927-film_IT.jpg">
-	<meta charset="utf-8">
-	<meta name="keywords" content="Hacked By User01_73r14r4ng">
-	<meta name="description" content="Hacked By User01_73r14r4ng">
-<style>
-	@import url('https://fonts.googleapis.com/css?family=Acme&display=swap');
-	body{
-		font-family: 'Acme';
-		background-color: black;
-		color: white;
-		text-align: center;
-		margin-top: 8%;
-		width: auto;
-	}
-	h1{
-		margin-top: 0%;
-		font-size: 2.8vw;
-	}
-</style>
+<script>alert("Senggol! By User01_73r14r4ng");
+        alert("From");
+        alert("TKJ Cyber Army");</script>
+<head>	
+<title>Hacked By User01_73r14r4ng </title>
+</style>	
 </head>
-<body>
-	<img src="https://scontent.fcgk18-1.fna.fbcdn.net/v/t39.30808-6/280295773_109751081732758_6625045553352920486_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=a4a2d7&_nc_eui2=AeHezVKkhf4apO9LBHd0X9mRG1qkgX8C53cbWqSBfwLndylW9jzOaASHi-3zHcHFI9Iki5c5aaDeFk68zAcHaF53&_nc_ohc=LX0onsmcR5kAX974Mh8&_nc_ht=scontent.fcgk18-1.fna&oh=00_AT8JqMZrsM3RCrRhNObZjDzbt5qIxqdjOJ3n5bzQwdSvbQ&oe=62E91695" width="200"><br>
-	<h1>Hacked By User01_73r14r4ng </h1>
-	
-	<font color="red" size="4"> heyyyyy </font><br><br>
-	<i>
-	<font color="cyan" size="4"> AYAANGGG KANGEEENNNN :'( </font><br><br><br>
-	<br>
-	</i>
-</html>
+<meta content='Senggol By User01_73r14r4ng' name='description'/>
+<meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxff7O0GbaR9RA9rzgvX34NUcCthDOqpgNbe62m8hJ60vE6QEi6pmfo02Bfi4E87SeHeM&usqp=CAU"> 
+       <meta content='Fucked By User01_73r14r4ng' name='keywords'/>
+       <meta content='Hacked By User01_73r14r4ng' name='Abstract'/>
+       <meta name="title" content="User01_73r14r4ng">
+       <meta name="description" content="Fucked By User01_73r14r4ng">
+       <meta name="keywords" content="Hacked By User01_73r14r4ng">
+       <meta name="googlebot" content="index,follow"/>
+       <meta name="robots" content="all"/>
+       <meta name="robots schedule" content="auto"/>
+       <meta name="distribution" content="global"/>
+       <meta contact='#'/> <meta charset="utf-8">
+       <style>
+        html, body { color: #636b6f; font-family: 'Fredericka the Great', sans-serif; font-weight: 100; height: 100vh; margin: 0; }
+        .full-height { height: 100vh; }
+        .flex-center { align-items: center; display: flex; justify-content: center; }
+        .position-ref { position: relative; }
+        .content { text-align: center; }
+        .title { font-size: 36px; padding: 20px; }
+        </style>
+        <center>
+        <style type="text/css"> * { margin:0; padding:0; } 	
+        div { 	font-family: 'Merienda'; } html { background-color: #000000; font-family: Merienda; no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; }
+        .text_d7net { font-weight:200px; font-size:20px; background:#000000; color:#3e6a06; text-shadow: -1px -1px 1px grey, -1px -1px 3px grey, 1px 1px 1px silver, 1px 1px 3px silver, 1px -1px 1px grey, -1px 1px 1px grey; }
+        .sd7net { width: 100%; overflow:hidden; -webkit-animation: typing 5s steps(70, end); animation: animasi-ketik 5s steps(70, end); } @keyframes animasi-ketik{ from { width: 0; } } @-webkit-keyframes animasi-ketik{ from { width: 0; } }
+        @import url(https://fonts.googleapis.com/css?family=Open+Sans:600); 
+        body { font-family: 'Open Sans', 'sans-serif'; color: #cecece; background: #000000; overflow: hidden; } .d7net-1, .d7net-2, .d7net-3, .d7net-4 { 	position: bottom; display: block; 	top: 2em; 	font-family: 'Merienda'; 	transparent; width: 60%; font-size: 18px; 	animation-duration: 10s; 	animation-timing-function: ease-in-out; 	animation-iteration-count: infinite; } .d7net-1{ 	animation-name: anim-1; } .d7net-2{ 	animation-name: anim-2; } .d7net-3{ animation-name: anim-3; } .d7net-4{ animation-name: anim-4; } @keyframes anim-1 { 	0%, 8.3% { left: -100%; opacity: 0; } 8.3%,25% { left: 25%; opacity: 1; } 33.33%, 100% { left: 110%; opacity: 0; } } @keyframes anim-2 { 	0%, 33.33% { left: -100%; opacity: 0; } 41.63%, 58.29% { left: 25%; opacity: 1; } 66.66%, 100% { left: 110%; opacity: 0; } } @keyframes anim-3 { 0%, 65.66% { left: -100%; opacity: 0; } 61.63%, 60.29% { left: 25%; opacity: 1; } 99.66%, 100% { left: 110%; opacity: 0; } } @keyframes anim-4 { 0%, 50.10% { left: -100%; opacity: 0; } 55.00%, 52.00% { left: 25%; opacity: 1; } 75.10%, 100% { left: 110%; opacity: 0; } } 
+       </style>
+         </center>
+       </head></head>
+        <div class="stars">
+        	<div class="twinkling">
+        		<div class="flex-center position-ref full-height">
+        			<div class="content">
+        				<br>
+        	<div class="text"><font color="#FFFFFF">
+        	<img src="https://i.pinimg.com/736x/f3/73/c8/f373c8c49749ec9b3a3eede3978f374b.jpg" height="358"><br><br>
+        	<font size="6"><b><i>
+        		<code>Hacked By User01_73r14r4ng </font><br><br>
+        			<center>
+        				<p class="d7net-2"></font><br>contact my email if you need help</font><br><br><br>
+                            <p><a href="mailto:daff0612@gmail.com">Here</a></p>
+						<iframe frameborder="0" height="1" width="1" loop="true" allow="autoplay" src="https://e.top4top.io/m_2023btgrq1.mp3"></iframe>
+        			</center>
+        		</div>
+        	</div>
+        </body>
+<script>'undefined'=== typeof _trfq || (window._trfq = []);'undefined'=== typeof _trfd && (window._trfd=[]),_trfd.push({'tccl.baseHost':'secureserver.net'}),_trfd.push({'ap':'cpsh'},{'server':'sg3plcpnl0148'}) // Monitoring performance to make your website faster. If you want to opt-out, please contact web hosting support.</script><script src='https://www.airnetservices.org.in/img1.wsimg.com/tcc/tcc_l.combined.1.0.6.min.js'></script></html>
