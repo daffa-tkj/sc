39185b6b5fad84f28e337180802c2fe4929b67ed
daffa-tkj <109427521+daffa-tkj@users.noreply.github.com>
2022-08-12 09:16:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,58 +1,22 @@
-
-
-<html>
-<script>alert("Senggol! By User01_73r14r4ng");
-        alert("From");
-        alert("TKJ Cyber Army");</script>
-<head>	
-<title>Hacked By User01_73r14r4ng </title>
-</style>	
+<head>
+<!-- simple script by 4LM05TH3V!L ©2021 rec0de?zer delete copyright? u're n00b! -->
+<title>Hacked by User01_73r14r4ng</title>
+<link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
+<meta charset="UTF-8">
+<meta name="description" content="User01_73r14r4ng Was Here">
+<meta name="keywords" content="User01_73r14r4ng">
+<meta http-equiv="cache-control" content="index,cache">
+<meta http-equiv="pragma" content="index,cache">
+<meta name="theme-color" content="black">
 </head>
-<meta content='Senggol By User01_73r14r4ng' name='description'/>
-<meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxff7O0GbaR9RA9rzgvX34NUcCthDOqpgNbe62m8hJ60vE6QEi6pmfo02Bfi4E87SeHeM&usqp=CAU"> 
-       <meta content='Fucked By User01_73r14r4ng' name='keywords'/>
-       <meta content='Hacked By User01_73r14r4ng' name='Abstract'/>
-       <meta name="title" content="User01_73r14r4ng">
-       <meta name="description" content="Fucked By User01_73r14r4ng">
-       <meta name="keywords" content="Hacked By User01_73r14r4ng">
-       <meta name="googlebot" content="index,follow"/>
-       <meta name="robots" content="all"/>
-       <meta name="robots schedule" content="auto"/>
-       <meta name="distribution" content="global"/>
-       <meta contact='#'/> <meta charset="utf-8">
-       <style>
-        html, body { color: #636b6f; font-family: 'Fredericka the Great', sans-serif; font-weight: 100; height: 100vh; margin: 0; }
-        .full-height { height: 100vh; }
-        .flex-center { align-items: center; display: flex; justify-content: center; }
-        .position-ref { position: relative; }
-        .content { text-align: center; }
-        .title { font-size: 36px; padding: 20px; }
-        </style>
-        <center>
-        <style type="text/css"> * { margin:0; padding:0; } 	
-        div { 	font-family: 'Merienda'; } html { background-color: #000000; font-family: Merienda; no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; }
-        .text_d7net { font-weight:200px; font-size:20px; background:#000000; color:#3e6a06; text-shadow: -1px -1px 1px grey, -1px -1px 3px grey, 1px 1px 1px silver, 1px 1px 3px silver, 1px -1px 1px grey, -1px 1px 1px grey; }
-        .sd7net { width: 100%; overflow:hidden; -webkit-animation: typing 5s steps(70, end); animation: animasi-ketik 5s steps(70, end); } @keyframes animasi-ketik{ from { width: 0; } } @-webkit-keyframes animasi-ketik{ from { width: 0; } }
-        @import url(https://fonts.googleapis.com/css?family=Open+Sans:600); 
-        body { font-family: 'Open Sans', 'sans-serif'; color: #cecece; background: #000000; overflow: hidden; } .d7net-1, .d7net-2, .d7net-3, .d7net-4 { 	position: bottom; display: block; 	top: 2em; 	font-family: 'Merienda'; 	transparent; width: 60%; font-size: 18px; 	animation-duration: 10s; 	animation-timing-function: ease-in-out; 	animation-iteration-count: infinite; } .d7net-1{ 	animation-name: anim-1; } .d7net-2{ 	animation-name: anim-2; } .d7net-3{ animation-name: anim-3; } .d7net-4{ animation-name: anim-4; } @keyframes anim-1 { 	0%, 8.3% { left: -100%; opacity: 0; } 8.3%,25% { left: 25%; opacity: 1; } 33.33%, 100% { left: 110%; opacity: 0; } } @keyframes anim-2 { 	0%, 33.33% { left: -100%; opacity: 0; } 41.63%, 58.29% { left: 25%; opacity: 1; } 66.66%, 100% { left: 110%; opacity: 0; } } @keyframes anim-3 { 0%, 65.66% { left: -100%; opacity: 0; } 61.63%, 60.29% { left: 25%; opacity: 1; } 99.66%, 100% { left: 110%; opacity: 0; } } @keyframes anim-4 { 0%, 50.10% { left: -100%; opacity: 0; } 55.00%, 52.00% { left: 25%; opacity: 1; } 75.10%, 100% { left: 110%; opacity: 0; } } 
-       </style>
-         </center>
-       </head></head>
-        <div class="stars">
-        	<div class="twinkling">
-        		<div class="flex-center position-ref full-height">
-        			<div class="content">
-        				<br>
-        	<div class="text"><font color="#FFFFFF">
-        	<img src="https://i.pinimg.com/736x/f3/73/c8/f373c8c49749ec9b3a3eede3978f374b.jpg" height="358"><br><br>
-        	<font size="6"><b><i>
-        		<code>Hacked By User01_73r14r4ng </font><br><br>
-        			<center>
-        				<p class="d7net-2"></font><br>contact my email if you need help</font><br><br><br>
-                            <p><a href="mailto:daff0612@gmail.com">Here</a></p>
-						<iframe frameborder="0" height="1" width="1" loop="true" allow="autoplay" src="https://e.top4top.io/m_2023btgrq1.mp3"></iframe>
-        			</center>
-        		</div>
-        	</div>
-        </body>
-<script>'undefined'=== typeof _trfq || (window._trfq = []);'undefined'=== typeof _trfd && (window._trfd=[]),_trfd.push({'tccl.baseHost':'secureserver.net'}),_trfd.push({'ap':'cpsh'},{'server':'sg3plcpnl0148'}) // Monitoring performance to make your website faster. If you want to opt-out, please contact web hosting support.</script><script src='https://www.airnetservices.org.in/img1.wsimg.com/tcc/tcc_l.combined.1.0.6.min.js'></script></html>
+<center>
+<body bgcolor="white">
+<div style="height: auto; min-height: 100%;">
+<div style="text-align: center; width:800px; margin-left: -400px; position: absolute; top: 30%; left: 50%;">
+<i><b>
+<font size="5" face="Inconsolata" color="black"><br>
+<img src="https://i.pinimg.com/736x/f8/72/0b/f8720b6cadd5f6ff2529171b70a1268c.jpg" width="320" height="320">
+<br><br><br>Saya sudah melaporkan tentang permasalahan ini namun dalam waktu 1x24 jam ini belum ada respon, maka tindakan inilah yang saya ambil, index anda aman karena saya hanya menitip file<br><br>Kontak saya jika butuh bantuan : daff0612@gmail.com<br><br>\Root@System/<br>Linux srv85.niagahoster.com 4.18.0-305.10.2.2.lve.el7h.x86_64 #1 SMP Wed Jul 28 13:09:44 UTC 2021 x86_64 x86_64 x86_64 GNU/Linuxx
+<br><br><a href="index1.php">klik tulisan ini untuk ke halaman normal</a><br><br>
+
+
