de804f68272c5a27f320a0433e8121702e2253fc
daffa-tkj <109427521+daffa-tkj@users.noreply.github.com>
2022-08-14 23:06:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,8 @@
 <i><b>
 <font size="5" face="Inconsolata" color="black"><br>
 <img src="https://i.pinimg.com/736x/f8/72/0b/f8720b6cadd5f6ff2529171b70a1268c.jpg" width="320" height="320">
-<br><br><br>Saya sudah melaporkan tentang permasalahan ini namun dalam waktu 1x24 jam ini belum ada respon, maka tindakan inilah yang saya ambil, index anda aman karena saya hanya menitip file<br><br>Kontak saya jika butuh bantuan : daff0612@gmail.com<br><br>\Root@System/<br>Linux srv85.niagahoster.com 4.18.0-305.10.2.2.lve.el7h.x86_64 #1 SMP Wed Jul 28 13:09:44 UTC 2021 x86_64 x86_64 x86_64 GNU/Linuxx
-<br><br><a href="index1.php">klik tulisan ini untuk ke halaman normal</a><br><br>
+<br><br><br>website anda memiliki kerentanan, namun jangan panik karena saya tidak mengubah file bawaan sistem ini, saya hanya menitipkan file ini saja<br><br>hubungi kontak saya jika membutuhkan bantuan : daff0612@gmail.com<br><br>\Root@System/<br><br>Linux srv118.niagahoster.com 4.18.0-305.10.2.2.lve.el7h.x86_64 #1 SMP Wed Jul 28 13:09:44 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux
+<br><br>uid=2639(u1275440) gid=2644(u1275440) groups=2644(u1275440)<br><br>
+
 
 
