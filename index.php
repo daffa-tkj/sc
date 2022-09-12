da79863be9ca2b70402c4515305aea2838b291a5
daffa-tkj <109427521+daffa-tkj@users.noreply.github.com>
2022-09-12 07:58:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,27 @@
-
-<html>
 <head>
-<title>..:: Terhacked::..</title>
-<meta property="og:title" content="# User01_73r14r4ng Was Here #"/> 
-<meta property="og:description" content="Defaced By User01_73r14r4ng"/>
-<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
-<meta property="og:image" content="https://pbs.twimg.com/profile_images/877205650224406528/_bDotn_T_400x400.jpg"> 
-<meta name="description" content="User01_73r14r4ng"> <meta name="keywords" content="User01_73r14r4ng">
+<!-- simple script by 4LM05TH3V!L ©2021 rec0de?zer delete copyright? u're n00b! -->
+<title>Hacked by User01_73r14r4ng</title>
+<link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
+<meta charset="UTF-8">
+<meta name="description" content="User01_73r14r4ng Was Here">
+<meta name="keywords" content="User01_73r14r4ng">
 <meta http-equiv="cache-control" content="index,cache">
 <meta http-equiv="pragma" content="index,cache">
-<style> 
-@import url('https://fonts.googleapis.com/css?family=Grand Hotel'); 
-@import url('https://fonts.googleapis.com/css?family=Germania One'); @import url('https://fonts.googleapis.com/css?family=Grand Hotel'); @import url('https://fonts.googleapis.com/css?family=Titan One');img{opacity:3;-webkit-transition:all 250ms ease;-moz-transition:all 250ms ease;-o-transition:all 250ms ease;transition:all 250ms ease} </style></style>
-<script src="http://e-mete.com/js/kdsnow.js" type="text/javascript"></script>
-<script src="../cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/daun-berguguran.js" type="text/javascript"></script>
-<br>
+<meta name="theme-color" content="black">
 </head>
- <body bgcolor="black" oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;'>
- <center>
- <img src='https://i.gifer.com/origin/40/40347e3f2bc01b8790a3c7278f39447b_w200.gif' height='150' title='Solo'/>
- </center>
- <br>
- <center>
-  <font face="Germania One" size="7" color="silver"><b>Hacked By User01_73r14r4ng </font><br>
- <img src='https://animesher.com/orig/0/91/919/9191/animesher.com_dark-anime-girl-demon-919168.jpg' height='400' title='Solo'/><br>
+<center>
+<body bgcolor="white">
+<div style="height: auto; min-height: 100%;">
+<div style="text-align: center; width:800px; margin-left: -400px; position: absolute; top: 30%; left: 50%;">
+<i><b>
+<font size="5" face="Inconsolata" color="black"><br>
+Hacked By User01_73r14r4ng<br><br>
+<img src="https://64.media.tumblr.com/79d61dad70b083aa25ffafbd5450fed6/380b2204b20a82df-f5/s540x810/a2689a50dfcc74b67611e9d8a4b8bd4c6f7f60d4.gifv" width="320" height="320">
+<br><br><br>kira-kira lulus nggak ya lamaran gw ?, capek banget ovt terus<br><br>\localhost@System/<br><br>Linux srv145.niagahoster.com 4.18.0-348.7.1.lve.el7h.x86_64 #1 SMP Fri Dec 24 19:28:54 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux
+<br><br>uid=1676(u1771324) gid=1681(u1771324) groups=1681(u1771324)<br><br>
+<br><br>
 <br>
- <font face="Indie Flower" size="5" color="silver">Message:<br>
-Sorry Admin ,, <br>
-Where is your security? <br></font>
- </body>
- </html>
+Thanks : InV!s1bl3 - G1br4N - Ind1h0m3 - IdoYkece<br><br><br>
+
+
+
