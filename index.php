00585413528a681ca65f446c61025472b75811ea
daffa-tkj <109427521+daffa-tkj@users.noreply.github.com>
2022-09-12 09:48:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,8 +17,7 @@
 <font size="5" face="Inconsolata" color="black"><br>
 Hacked By User01_73r14r4ng<br><br>
 <img src="https://64.media.tumblr.com/79d61dad70b083aa25ffafbd5450fed6/380b2204b20a82df-f5/s540x810/a2689a50dfcc74b67611e9d8a4b8bd4c6f7f60d4.gifv" width="320" height="320">
-<br><br><br>kira-kira lulus nggak ya lamaran gw ?, capek banget ovt terus<br><br>\localhost@System/<br><br>Linux srv145.niagahoster.com 4.18.0-348.7.1.lve.el7h.x86_64 #1 SMP Fri Dec 24 19:28:54 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux
-<br><br>uid=1676(u1771324) gid=1681(u1771324) groups=1681(u1771324)<br><br>
+<br><br><br>kira-kira lulus nggak ya lamaran gw ?, capek banget ovt terus<br><br>
 <br><br>
 <br>
 Thanks : InV!s1bl3 - G1br4N - Ind1h0m3 - IdoYkece<br><br><br>
